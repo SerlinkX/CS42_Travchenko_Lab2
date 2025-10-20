a1dab4b456d6b91bfb48f6e80e6ac2c08ad8cb0d
SerlinkX <162686136+SerlinkX@users.noreply.github.com>
2025-10-20 12:09:02 +0300

    Commit Message

--- src/main/java/com/example/lab1/Lab1Application.java
+++ src/main/java/com/example/lab1/Lab1Application.java
@@ -41,7 +41,7 @@ public class Lab1Application implements CommandLineRunner {
 
 
         int max = maxNumberService.findMax(numbers);
-        System.out.println("Максимальне число: " + max);
+        System.out.println("Максимальніше число: " + max);
     }
 }
 
